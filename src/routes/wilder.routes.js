7f6f59db4da73a6d87ad1567fef087fa21ef4d61
David <d.elbaze@gmail.com>
2023-03-07 11:36:59 +0100

    list, update, delete and patch

--- src/routes/wilder.routes.js
+++ src/routes/wilder.routes.js
@@ -21,9 +21,60 @@ router.post("/create", async (req, res) => {
   }
 });
 
-router.get("/list", (req, res) => {});
-router.delete("/delete/:id", (req, res) => {});
-router.patch("/update/:id", (req, res) => {});
+router.get("/list", async (req, res) => {
+  try {
+    const wilderList = await new WilderService().list();
+    res.json(wilderList);
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+router.get("/find/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const wilder = await new WilderService().findById(id);
+    res.json(wilder);
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+router.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await new WilderService().delete(id);
+    res.json(result);
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+router.patch("/update/:id", async (req, res) => {
+  const { id } = req.params;
+  const { first_name, last_name, email } = req.body;
+
+  try {
+    const wilder = await new WilderService().update({
+      id,
+      first_name,
+      last_name,
+      email,
+    });
+    res.json(wilder);
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 // router
 //   .route("/create")
 //   .post((req, res) => {})
--- src/services/Wilder.service.js
+++ src/services/Wilder.service.js
@@ -10,4 +10,36 @@ export default class WilderService {
     const wilder = this.db.create({ first_name, last_name, email });
     return await this.db.save(wilder);
   }
+
+  async list() {
+    return await this.db.find();
+  }
+
+  async findById(id) {
+    let wilder = await this.db.findOneBy({ id });
+    if (!wilder) {
+      throw new Error("Ce wilder n'existe pas");
+    }
+    return wilder;
+  }
+
+  async delete(id) {
+    let result = await this.db.delete({ id });
+    if (result.affected === 0) {
+      throw new Error("Problème, ce wilder n'existe peut être pas?");
+    }
+    return {
+      success: true,
+      message: "Wilder supprimé",
+    };
+  }
+  async update({ id, ...other }) {
+    let wilder = await this.findById(id);
+    Object.keys(other).forEach((value) => {
+      if (other[value]) {
+        wilder[value] = other[value];
+      }
+    });
+    return await this.db.save(wilder);
+  }
 }
