e0ba96d4b2302077f92c95eececd26b1fdda57b0
David <d.elbaze@gmail.com>
2023-03-08 15:46:07 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import datasource from "./lib/datasource";
 import maFonction from "./lib/utilities";
 import wilderRoutes from "./routes/wilder.routes";
 import languageRoutes from "./routes/language.routes";
+import noteRoutes from "./routes/note.routes";
 const app = express();
 
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/wilder", wilderRoutes);
 app.use("/language", languageRoutes);
+app.use("/note", noteRoutes);
 
 const start = async () => {
   await datasource.initialize();
